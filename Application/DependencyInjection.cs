50c7cd4dbb0054515e61e340181a7f9739243f9f
Ahmad Hamwi <ahmadhamwi999@gmail.com>
2022-04-22 18:02:01 +0300

    Adds Application invoicing gateways

--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -10,15 +10,28 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        services.AddMediatR(Assembly.GetExecutingAssembly());
+        return services
+            .AddMediatR(Assembly.GetExecutingAssembly())
+            .AddLazyDi();
+    }
+
+    private static IServiceCollection AddLazyDi(this IServiceCollection services)
+    {
+        services.AddTransient(typeof(Lazy<>), typeof(LazyDi<>));
         return services;
     }
 
     public static void AddApplicationAutomapper(this IServiceCollection services, Assembly[] assemblies)
     {
-        services.AddAutoMapper(config =>
+        services.AddAutoMapper(config => { config.AddProfile(new MappingProfiles(assemblies)); });
+    }
+
+    // ReSharper disable once ConvertClosureToMethodGroup
+    internal class LazyDi<T> : Lazy<T> where T : class
+    {
+        public LazyDi(IServiceProvider serviceProvider)
+            : base(() => serviceProvider.GetRequiredService<T>())
         {
-            config.AddProfile(new MappingProfiles(assemblies));
-        });
+        }
     }
 }
---
+++ Application/Repositories/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+namespace Application.Repositories.UnitOfWork;
+
+public interface IUnitOfWork
+{
+    public IAccountRepository AccountRepository { get; }
+    public ICategoryRepository CategoryRepository { get; }
+    public ICurrencyAmountRepository CurrencyAmountRepository { get; }
+    public ICurrencyRepository CurrencyRepository { get; }
+    public IInvoiceRepository InvoiceRepository { get; }
+    public IManufacturerRepository ManufacturerRepository { get; }
+    public IProductMovementRepository ProductMovementRepository { get; }
+    public IProductRepository ProductRepository { get; }
+    public IStoragePlaceRepository StoragePlaceRepository { get; }
+    public IUnitRepository UnitRepository { get; }
+    public IUserRepository UserRepository { get; }
+    public IWarehouseRepository WarehouseRepository { get; }
+
+    public void SaveChanges();
+}
