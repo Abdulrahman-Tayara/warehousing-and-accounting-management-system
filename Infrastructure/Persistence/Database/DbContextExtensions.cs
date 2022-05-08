2ab3770ad94144bd7f3004db808f62991d71f0ef
AbdulrahmanTayara <teara291@gmail.com>
2022-05-08 20:43:46 +0300

    Add global query filter for soft deleteable entity

---
+++ Infrastructure/Persistence/Database/DbContextExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace Infrastructure.Persistence.Database;
+
+public static class DbContextExtensions
+{
+    public static void ApplyGlobalFilters<TInterface>(this ModelBuilder modelBuilder, Expression<Func<TInterface, bool>> expression)
+    {
+        var entities = modelBuilder.Model
+            .GetEntityTypes()
+            .Where(e => e.ClrType.GetInterface(typeof(TInterface).Name) != null)
+            .Select(e => e.ClrType);
+        
+        foreach (var entity in entities)
+        {
+            var newParam = Expression.Parameter(entity);
+            var newbody = ReplacingExpressionVisitor.Replace(expression.Parameters.Single(), newParam, expression.Body);    
+            modelBuilder.Entity(entity).HasQueryFilter(Expression.Lambda(newbody, newParam));
+        }
+    }
+}
