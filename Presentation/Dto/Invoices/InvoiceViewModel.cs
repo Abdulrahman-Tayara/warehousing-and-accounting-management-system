361bb690a3686d64fbee042b9e271edd099c9fe2
Ahmad Hamwi <ahmadhamwi999@gmail.com>
2022-05-20 08:55:13 +0300

    Adds nav properties to the invoice viewmodel

--- Presentation/Dto/Invoices/InvoiceViewModel.cs
+++ Presentation/Dto/Invoices/InvoiceViewModel.cs
@@ -1,15 +1,18 @@
 ﻿using Application.Common.Mappings;
 using Domain.Entities;
+using wms.Dto.Accounts;
 using wms.Dto.Common;
+using wms.Dto.Currencies;
+using wms.Dto.Warehouses;
 
 namespace wms.Dto.Invoices;
 
 public class InvoiceViewModel : IViewModel, IMapFrom<Invoice>
 {
     public int Id { get; set; }
-    public int AccountId { get; set; }
-    public int WarehouseId { get; set; }
-    public int? CurrencyId { get; set; }
+    public AccountViewModel Account { get; set; } = default!;
+    public WarehouseViewModel Warehouse { get; set; } = default!;
+    public CurrencyViewModel? Currency { get; set; }
     public double TotalPrice { get; set; }
     public string? Note { get; set; }
     public DateTime CreatedAt { get; set; }
