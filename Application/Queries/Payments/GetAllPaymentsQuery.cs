3880988d9d9d30c9342d277e70a33ee4f6b30655
Ahmad Hamwi <ahmadhamwi999@gmail.com>
2022-05-21 17:06:13 +0300

    Adds Payment filters in get all request

--- Application/Queries/Payments/GetAllPaymentsQuery.cs
+++ Application/Queries/Payments/GetAllPaymentsQuery.cs
@@ -6,7 +6,11 @@ namespace Application.Queries.Payments;
 
 public class GetAllPaymentsQuery : GetPaginatedQuery<Payment>
 {
-    public int InvoiceId { get; set; }
+    public int InvoiceId { get; set; } = 0;
+
+    public PaymentType? PaymentType { get; set; } = default;
+
+    public PaymentIoType? PaymentIoType { get; set; } = default;
 }
 
 public class GetAllPaymentsQueryHandler : PaginatedQueryHandler<GetAllPaymentsQuery, Payment>
@@ -21,10 +25,13 @@ public class GetAllPaymentsQueryHandler : PaginatedQueryHandler<GetAllPaymentsQu
     protected override Task<IQueryable<Payment>> GetQuery(GetAllPaymentsQuery request,
         CancellationToken cancellationToken)
     {
-        return Task.FromResult(
-            _paymentRepository
-                .GetAll(new GetAllOptions<Payment> {IncludeRelations = true})
-                .Where(payment => payment.InvoiceId == request.InvoiceId || request.InvoiceId == default)
-        );
+
+        var query = _paymentRepository
+            .GetAll(new GetAllOptions<Payment> {IncludeRelations = true})
+            .Where(payment => payment.InvoiceId == request.InvoiceId || request.InvoiceId == default)
+            .Where(payment => payment.PaymentType == request.PaymentType || request.PaymentType == default)
+            .Where(payment => payment.PaymentIoType == request.PaymentIoType || request.PaymentIoType == default);
+        
+        return Task.FromResult(query);
     }
 }
--- Presentation/Dto/Payments/GetAllPaymentsRequest.cs
+++ Presentation/Dto/Payments/GetAllPaymentsRequest.cs
@@ -1,5 +1,6 @@
 using Application.Common.Mappings;
 using Application.Queries.Payments;
+using Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 using wms.Dto.Pagination;
 
@@ -7,5 +8,9 @@ namespace wms.Dto.Payments;
 
 public class GetAllPaymentsRequest : PaginationRequestParams, IMapFrom<GetAllPaymentsQuery>
 {
-    [FromQuery] public int InvoiceId { get; set; } = default!;
+    [FromQuery] public int InvoiceId { get; set; } = default;
+
+    [FromQuery] public PaymentType? PaymentType { get; set; } = default;
+
+    [FromQuery] public PaymentIoType? PaymentIoType { get; set; } = default;
 }
