a1ce4699d09ba993c4e43f91d39e551a76b2f42a
AbdulrahmanTayara <teara291@gmail.com>
2022-05-21 16:30:06 +0300

    Add create invoice validator

---
+++ Application/Commands/Invoicing/CreateInvoiceCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Application.Commands.Invoicing;
+
+public class CreateInvoiceCommandValidator : AbstractValidator<CreateInvoiceCommand>
+{
+    public CreateInvoiceCommandValidator()
+    {
+        RuleFor(command => command.Items)
+            .NotEmpty().WithMessage("Can't create empty invoice");
+    }
+}
