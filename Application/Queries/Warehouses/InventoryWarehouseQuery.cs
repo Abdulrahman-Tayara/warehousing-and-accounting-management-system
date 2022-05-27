e653dbb2c5697acdb59f0498ce3796d582363edb
Ahmad Hamwi <ahmadhamwi999@gmail.com>
2022-05-27 21:25:51 +0300

    Refactors inventory query handler to execute only the repository query

--- Application/Queries/Warehouses/InventoryWarehouseQuery.cs
+++ Application/Queries/Warehouses/InventoryWarehouseQuery.cs
@@ -3,7 +3,6 @@ using Application.Common.Models;
 using Application.Queries.Common;
 using Application.Repositories;
 using Domain.Aggregations;
-using Domain.Entities;
 using MediatR;
 
 namespace Application.Queries.Warehouses;
@@ -17,38 +16,18 @@ public class InventoryWarehouseQueryHandler : IRequestHandler<InventoryWarehouse
     IPaginatedEnumerable<AggregateProductQuantity>>
 {
     private readonly IProductMovementRepository _productMovementRepository;
-    private readonly IProductRepository _productRepository;
 
-    public InventoryWarehouseQueryHandler(IProductMovementRepository productMovementRepository,
-        IProductRepository productRepository)
+    public InventoryWarehouseQueryHandler(IProductMovementRepository productMovementRepository)
     {
         _productMovementRepository = productMovementRepository;
-        _productRepository = productRepository;
     }
 
     public Task<IPaginatedEnumerable<AggregateProductQuantity>> Handle(InventoryWarehouseQuery request,
         CancellationToken cancellationToken)
     {
-        var aggregates = _productMovementRepository
-            .AggregateProductsQuantities(request.Filters)
-            .ToList();
-
-        var productIds = aggregates
-            .Select(aggregate => aggregate.Product!.Id);
-
-        var products = _productRepository
-            .GetAll(new GetAllOptions<Product> {IncludeRelations = true})
-            .Where(product => productIds.Any(id => product.Id == id))
-            .ToList();
-
-        var aggregatesWithFullProduct = aggregates
-            .Zip(products)
-            .Select(entry => entry.First.AddProduct(entry.Second));
-
-        var paginatedQuery = aggregatesWithFullProduct
-            .AsQueryable()
-            .AsPaginatedQuery(request.Page, request.PageSize);
-
-        return Task.FromResult(paginatedQuery);
+        return Task.FromResult(
+            _productMovementRepository.AggregateProductsQuantities(request.Filters)
+                .AsPaginatedQuery(request.Page, request.PageSize)
+        );
     }
 }
