52195d04addb61107f01c484fa7b67632326e2bf
Ahmad Hamwi <ahmadhamwi999@gmail.com>
2022-07-23 20:22:07 +0300

    Adds product model to notification view model.

--- Presentation/Controllers/Api/NotificationsController.cs
+++ Presentation/Controllers/Api/NotificationsController.cs
@@ -1,12 +1,14 @@
+using Application.Common.Models;
 using Application.Queries.Notifications;
+using Application.Queries.Products;
 using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using wms.Dto.Common;
 using wms.Dto.Common.Responses;
 using wms.Dto.Notifications;
 using wms.Dto.Pagination;
+using wms.Dto.Products;
 
 namespace wms.Controllers.Api;
 
@@ -16,16 +18,33 @@ public class NotificationsController : ApiControllerBase
     public NotificationsController(IMediator mediator, IMapper mapper) : base(mediator, mapper)
     {
     }
-    
+
     [HttpGet]
     public async Task<ActionResult<BaseResponse<PageViewModel<NotificationViewModel>>>> GetAll(
         [FromQuery] GetNotificationsQueryParams queryParams
-        )
+    )
     {
         var query = Mapper.Map<GetAllNotificationsQuery>(queryParams);
 
         var notificationsPage = await Mediator.Send(query);
 
-        return Ok(notificationsPage.ToViewModel<NotificationViewModel>(Mapper));
+        var productsQuery = new GetAllProductsQuery();
+        var products = (await Mediator.Send(productsQuery))
+            .AsEnumerable()
+            .Where(product => notificationsPage.Any(notification => notification.ObjectId == product.Id))
+            .Select(productEntity => Mapper.Map<ProductViewModel>(productEntity));
+
+        var notificationsViewModelPage = notificationsPage
+            .Select(notification => new NotificationViewModel
+            {
+                Id = notification.Id,
+                NotificationType = notification.NotificationType,
+                Product = products.First(product => product.Id == notification.ObjectId),
+                IsValid = notification.IsValid
+            })
+            .AsQueryable()
+            .AsPaginatedQuery(notificationsPage.CurrentPage, notificationsPage.PageSize);
+
+        return Ok(notificationsViewModelPage);
     }
 }
--- Presentation/Dto/Notifications/NotificationViewModel.cs
+++ Presentation/Dto/Notifications/NotificationViewModel.cs
@@ -1,14 +1,15 @@
 using Application.Common.Mappings;
 using Domain.Entities;
 using wms.Dto.Common;
+using wms.Dto.Products;
 
 namespace wms.Dto.Notifications;
 
 public class NotificationViewModel : IViewModel, IMapFrom<Notification>
 {
     public int Id { get; set; }
-    
-    public int ObjectId { get; set; }
+
+    public ProductViewModel Product { get; set; }
 
     public NotificationType NotificationType { get; set; }
 }
