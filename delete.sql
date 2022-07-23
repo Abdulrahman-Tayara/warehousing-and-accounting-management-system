1c7a6dbdc9cd408d0ecaaa635f55eb049b469e91
Ahmad Hamwi <ahmadhamwi999@gmail.com>
2022-07-23 20:20:42 +0300

    Adds delete data sql script

---
+++ delete.sql
@@ -0,0 +1,83 @@
+DELETE FROM [dbo].[JournalDbs]
+GO
+
+DELETE FROM [dbo].[Payments]
+GO
+
+DELETE FROM [dbo].[Movements]
+GO
+
+DELETE FROM [dbo].[Invoices]
+GO
+
+DELETE FROM [dbo].[Accounts]
+GO
+
+DELETE FROM [dbo].[Categories]
+GO
+
+DELETE FROM [dbo].[CountryOrigins]
+GO
+
+DELETE FROM [dbo].[Currencies]
+GO
+
+DELETE FROM [dbo].[Notifications]
+GO
+
+DELETE FROM [dbo].[Products]
+GO
+
+DELETE FROM [dbo].[Settings]
+GO
+
+DELETE FROM [dbo].[Units]
+GO
+
+DELETE FROM [dbo].[Warehouses]
+GO
+
+DELETE FROM [dbo].[CurrencyAmounts]
+GO
+
+DELETE FROM [dbo].[Payments]
+GO
+
+DELETE FROM [dbo].[Movements]
+GO
+
+DELETE FROM [dbo].[Invoices]
+GO
+
+DELETE FROM [dbo].[Accounts]
+GO
+
+DELETE FROM [dbo].[Categories]
+GO
+
+DELETE FROM [dbo].[CountryOrigins]
+GO
+
+DELETE FROM [dbo].[Currencies]
+GO
+
+DELETE FROM [dbo].[Notifications]
+GO
+
+DELETE FROM [dbo].[Products]
+GO
+
+DELETE FROM [dbo].[Settings]
+GO
+
+DELETE FROM [dbo].[Units]
+GO
+
+DELETE FROM [dbo].[Warehouses]
+GO
+
+DELETE FROM [dbo].[CurrencyAmounts]
+GO
+
+DELETE FROM [dbo].[Manufacturers]
+GO
